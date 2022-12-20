24b172751b18aea46568cc7d22b1e93d1755adf4
ELeentvaarDeltares <eline.leentvaar@deltares.nl>
2022-12-20 09:01:38 +0100

    change some things

--- pick_firstbreak.py
+++ pick_firstbreak.py
@@ -12,8 +12,24 @@ from obspy.signal.trigger import plot_trigger
 from obspy.signal.trigger import recursive_sta_lta
 from obspy.signal.trigger import pk_baer
 import glob
+import csv
+import pandas as pd
 
 #%% 
+
+def write_vs(filename,dist,tt,dx,n_sources,source_pos):
+
+    file = open(filename ,'w')
+    file.write('1996 0 3.000000\n0 %d %f\n'%(n_sources,dx))
+    
+    #for i in range(0,len(freq)):
+    file.write('%0.6f %d 0.000000\n'%(float(source_pos), len(tt)))  
+        
+    for i in range(0,len(dist)):    
+        file.write('%0.6f %0.6f 1 \n'%(dist[i],tt[i]))
+    file.write('0 0 \n 0 \n 0 0 \n')
+    file.close()
+    return
 
 def Pick_first_breaks(file,f,files):
     #Plot Signal
@@ -27,8 +43,8 @@ def Pick_first_breaks(file,f,files):
         df = trace.stats.sampling_rate/100
         
         #Plot calculated trigger start
-        cft = recursive_sta_lta(trace, int(5 * df), int(10 * df))
-        plot_trigger(trace, cft, 1.7, 0.5)
+       # cft = recursive_sta_lta(trace, int(5 * df), int(10 * df))
+       # plot_trigger(trace, cft, 1.7, 0.5)
         
         #Find locatin of trigger start in array
         p_pick, phase_info = pk_baer(trace, df, 20, 60, 7.0, 12.0, 100, 100)
@@ -37,11 +53,16 @@ def Pick_first_breaks(file,f,files):
             
         file.traces[iii].data = file.traces[iii].data[int(p_pick):]
             
+
     # Write output file
-    outfile = os.path.join(path, f"FBpicked{f}.csv") #two colomn file
+    #outfile = write_vs(path+"FBpicked"+str(f)+".vs",np.linspace(0,1,np.shape(file.traces)[0]),file.traces[0].data,1,4,file.traces[0].stats.seg2.SOURCE_LOCATION)
     
-    file.write(outfile)
-            
+    outfile = open(path+"FBpicked"+str(f)+".csv",'w') #two colomn file
+    writer = csv.writer(outfile)
+    
+    writer.writerow(file.traces)
+    outfile.close()
+        
     return 
 
 
@@ -58,6 +79,9 @@ for f in range(len(files)):
 #%%
 
 #plot cutted part weghalen later
-
-new = read('C:\\Users\\leentvaa\\PythonScripts\\Refraction_Seismic\\test0.segy')
-new.plot(type="relative")
+'''
+new = pd.read_csv('')
+with open('C:\\Users\\leentvaa\\PythonScripts\\Refraction_Seismic\\FBPicked1.csv', 'r') as file:
+    reader = csv.reader(file)
+reader.plot(type="relative")
+'''
